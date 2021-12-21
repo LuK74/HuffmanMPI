e4b2723dd6f6963ec58246b559c67f8a37aa1988
LuK74 <lucas.chaloyard@gmail.com>
2021-12-21 18:50:10 +0100

    MPI added, needs to be tested, and completed

--- HuffmanMPI/src/huffman.cpp
+++ HuffmanMPI/src/huffman.cpp
@@ -7,27 +7,45 @@
 int mpi_rank, mpi_size;
 
 s_table generate_table(char * filename, int * f_size) {
+  s_table table = init_table(0,0,NULL);
+  int total_occ = 0;
+  char c;
+
+  /*
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
   MPI_File fh;
   MPI_Status status;
   MPI_Offset file_size;
   MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
   MPI_File_get_size(fh, &file_size);
   
-  s_table table = init_table(0,0,NULL);
-  int total_occ = 0;
-  char c;
-  
-  char * buf = (char*) malloc(sizeof(char)*file_size);
-  
-  MPI_File_read_all(fh, buf, file_size, MPI_CHAR, &status);
-  for (int i = 0; i < file_size; i++) {
+  int to_read = file_size/mpi_size;
+  if (mpi_rank + 1 == mpi_size) {
+    to_read += (file_size%mpi_size);
+  }
+  */
+  //char * buf = (char*) malloc(sizeof(char)*to_read);
+  //MPI_File_read_at(fh, mpi_rank*to_read ,buf, to_read, MPI_CHAR, &status);
+
+  FILE * file = fopen(filename, "r");
+  c = fgetc(file);
+  while (c != EOF) {
+     total_occ++;
+     increment_entry(table, c);
+     c = fgetc(file);
+  }
+  /*for (int i = 0; i < to_read; i++) {
     c = buf[i];
     total_occ++;
     increment_entry(table, c);
-  }
-
+    }*/
+  //MPI_Finalize();
+  
   build_frequency(table, total_occ);
   *f_size = total_occ;
+
   
   return table;
 }
@@ -96,6 +114,129 @@ int compress(FILE * input, FILE * output, s_table table, tree root, int f_size) 
   
   return 0;
 }
+
+int compress_mpi(char * input, FILE * output, s_table table, tree root, int f_size) {
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+  MPI_File fh;
+  MPI_Status status;
+  MPI_Offset file_size;
+  MPI_File_open(MPI_COMM_WORLD, input, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+  MPI_File_get_size(fh, &file_size);
+  
+  int to_read = file_size/mpi_size;
+  if (mpi_rank + 1 == mpi_size) {
+    to_read += (file_size%mpi_size);
+  }
+  
+  char * input_buf = (char*) malloc(sizeof(char)*to_read);
+  MPI_File_read_at(fh, mpi_rank*to_read ,input_buf, to_read, MPI_CHAR, &status);
+  
+  int curr_size = 0;
+  char to_print = 0;
+  s_entry entry = NULL;
+
+  int buf_size = f_size;
+  char * buf = (char *) malloc(sizeof(char)*buf_size);
+  
+  int total_size = 0;
+  int total_occ = 0;
+
+  int index = 1;
+  char c = input_buf[0];
+  
+  while(index < to_read) {
+    total_occ++;
+    entry = get_entry(table, c);
+    if (entry == NULL) return -1;
+    for (int i = 0; entry->encoding[i] != '\0'; i++) {
+      if (curr_size != 0) to_print = to_print<<1;
+      
+      to_print = to_print | ((entry->encoding[i]) - '0');
+      curr_size++;
+
+      if (curr_size == 8) {
+	buf[total_size] = to_print;
+	to_print = 0;
+	curr_size = 0;
+
+	total_size++;
+	if (total_size >= buf_size) {
+	  char * old_buf = buf;
+	  buf = (char *) malloc(sizeof(char)*(buf_size*2));
+	  for (int i = 0; i < buf_size; i++) { buf[i] = old_buf[i] ; }
+	  buf_size *= 2;
+	  free(old_buf);
+	}
+      } //else {
+	//to_print = to_print<<1;
+      //}
+      
+    }
+    c = input_buf[index];
+    index++;
+  }
+
+  if (mpi_rank + 1 == mpi_size) {
+    char * final_buf;
+    int final_size = 0;
+    
+    int padding = 0;
+    if (curr_size != 0) {
+      padding = 8-curr_size;
+      to_print = to_print<<padding;
+      buf[total_size] = to_print;
+      total_size++;
+    }
+    
+    for (int i = 0; i < mpi_size - 1; i++) {
+      int buf_size = 0;
+      MPI_Recv(&buf_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      final_size += buf_size;
+      
+      char * r_buf = (char *) malloc(sizeof(char)*buf_size);
+      MPI_Recv(r_buf, buf_size, MPI_CHAR, i, 0, MPI_COMM_WORLD, &status); 
+
+      char * old_buf = final_buf;
+      final_buf = (char *) malloc(sizeof(char)*final_size);
+      for (int i = 0; i < final_size - buf_size; i++) { final_buf[i] = old_buf[i]; }
+      free(old_buf);
+
+      for (int i = final_size - buf_size; i < final_size; i++) {
+	final_buf[i] = r_buf[i - (final_size - buf_size)];
+      }
+    }
+
+    final_size += total_size;
+    char * old_buf = final_buf;
+    final_buf = (char *) malloc(sizeof(char)*final_size);
+    for (int i = 0; i < final_size - buf_size; i++) { final_buf[i] = old_buf[i]; }
+    free(old_buf);
+
+    for (int i = final_size - buf_size; i < final_size; i++) {
+      final_buf[i] = buf[i - (final_size - buf_size)];
+    }
+       
+    write_table(output, table, padding, total_occ, total_size);
+
+    for (int i = 0; i < total_size; i++) {
+      fputc(buf[i], output);
+    }
+    
+    free(buf);
+    
+  } else {
+    MPI_Send(&total_size, 1, MPI_INT, mpi_size - 1, 0, MPI_COMM_WORLD);
+    MPI_Send(buf, total_size, MPI_CHAR, mpi_size - 1, 0, MPI_COMM_WORLD);
+  } 
+  //char * buffer = malloc(sizeof(char)*total_size);
+  //read(tube[0], buffer, total_size);
+
+  
+  return 0;
+}
+
 
 int decompress(FILE * input, FILE * output) {
   rewind(input);
@@ -233,10 +374,6 @@ void process_args(int argc, char * argv[]) {
 }
 
 int main(int argc, char * argv[]) {
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
   process_args(argc, argv);
-  MPI_Finalize();
   return 0;
 }
